5bdd5eae56e158b22b1f99de2e423f96cb8d9e6d
Anna Axelsson <a.axelsson51@gmail.com>
2023-10-15 20:19:24 +0200

    Add carousel component and styling

---
+++ WeatherClient/Weather/src/components/Carousel.jsx
@@ -0,0 +1,70 @@
+import { useState, useRef, useEffect } from "react";
+import styled from "styled-components";
+
+
+const Carousel = ({ weatherData }) => {
+  const carousel = useRef(null);
+
+  console.log(weatherData);
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+    console.log(carousel.current.scrollLeft);
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+    console.log(carousel.current.scrollLeft);
+  };
+
+  return (
+    <>
+      <CarouselContainer id="bookmarks" className="carousel" ref={carousel}>
+        {weatherData.map((item, index) => {
+          return (
+            <CarouselItem key={index}>
+            </CarouselItem>
+          );
+        })}
+      </CarouselContainer>
+    </>
+  );
+};
+export default Carousel;
+
+const CarouselContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  overflow: hidden;
+  scroll-behavior: smooth;
+  margin: 0 auto;
+  width: 50%;
+  gap: 1rem;
+
+  > div {
+    display: flex;
+  }
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+
+  @media screen and (max-width: 1150px) {
+    width: 95%;
+  }
+`;
+
+const CarouselItem = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 500px;
+  justify-content: center;
+
+  @media screen and (max-width: 600px) {
+    align-items: left;
+  }
+`;
